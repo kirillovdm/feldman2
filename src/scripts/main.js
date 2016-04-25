c26b7363c0a4a367bf8f0af043b8bb3822a19b39
kirillovdm <kyrylovdm@gmail.com>
2016-04-25 14:37:44 +0300

    up

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,8 @@
 $(window).on('load', function () {
-    var $preloader = $('.page-preloader'),
-        $spinner   = $preloader.find('.preloader-itself');
-    $spinner.fadeOut();
-    $preloader.delay(350).fadeOut('slow');
+    var $preloader = $('.page-preloader');
+    $preloader.delay(50).fadeOut('slow', function(){
+        $('.head-title').addClass('fadeIn');
+    });   
 });
 
 
@@ -25,26 +25,27 @@ $(window).on('load', function () {
         }
     });
 
-    $('.carousel').carousel({
-      interval: 3000
-    })
+var wow = new WOW();
+wow.init();
+
+    // setTimeout(function(){
+    //         $('.modal').modal('show');
+    // }, 5000);
 
 
-
-
-TweenMax.staggerTo('#Fill-5', 3, {drawSVG:0, delay:1}, 0.1);
-
-// var tl = new TimelineMax({repeat:-1, repeatDelay:0.3, yoyo: true});
-// // MorphSVGPlugin.convertToPath('#vk-icon, #insta-icon, #fb-icon')
-// tl.to('#vk-icon', 1.3, {morphSVG:'#fb-icon'});
-
+     var isMobile = false; //initiate as false
+    // device detection
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
+    if (isMobile) {
+        $('.wow').removeClass('wow');
+    }
 
 
 
 })(window.jQuery);
 
-var wow = new WOW();
-wow.init();
+
 
 
 $(document).ready(function(){
@@ -57,6 +58,20 @@ $(document).ready(function(){
     speed: 1000,
     pager: false
   });
+
+  $('.main-bx-slider').bxSlider({
+    minSlides: 1,
+    maxSlides: 1,
+    moveSlides: 1,
+    slideMargin: 0,
+    auto: true,
+    speed: 1000,
+    pager: false,
+    controls:false,
+    mode: 'fade'
+  });
+
+  
   
   setTimeout(function(){
     $("div:contains('Thumbnail Slider trial version')").contents().filter(function () {
@@ -66,24 +81,121 @@ $(document).ready(function(){
         return (this.nodeType == 3 && $.trim(this.nodeValue) == 'Ninja Slider trial version');
     }).remove();
 
-        // $('.fotorama__arr').clone().appendTo('.fotorama__wrap');
-        // $('.fotorama__stage  .fotorama__arr').remove();
   }, 300);
 });
 
-function checkOffset(){
-    var screenHeight = window.innerHeight;
-    var windOffset = window.pageYOffset;
-    var $el = $('.spy-el')
-    var elOffset = $el.offset().top;
+// function checkOffset(){
+//     var screenHeight = window.innerHeight;
+//     var windOffset = window.pageYOffset;
+//     var $el = $('.spy-el')
+//     var elOffset = $el.offset().top;
 
-    if(windOffset > elOffset - screenHeight/1.5){
-        $('.spy-el').addClass('show-el');
-    }
-    requestAnimationFrame(checkOffset);
-}
-requestAnimationFrame(checkOffset);
+//     if(windOffset > elOffset - screenHeight/1.5){
+//         $('.spy-el').addClass('show-el');
+//     }
+//     requestAnimationFrame(checkOffset);
+// }
+
+
+// requestAnimationFrame(checkOffset);
 // $(window).on('scroll', function(){
 //     }
 // });
 
+
+
+$(document).on('click tap touchstart', '.menu-icon', function(){
+    $('.bottom-header, .menu-icon').toggleClass('active');
+});
+$(document).on('show.bs.modal hidden.bs.modal', '.modal', function(){
+    $('.bottom-header').toggleClass('open-modal');
+});
+
+$(function() {
+
+    var Page = (function() {
+
+        var $navArrows = $( '#nav-arrows' ),
+        $nav = $( '#nav-dots > span' ),
+        slitslider = $( '#slider' ).slitslider( {
+            onBeforeChange : function( slide, pos ) {
+
+                $nav.removeClass( 'nav-dot-current' );
+                $nav.eq( pos ).addClass( 'nav-dot-current' );
+
+            }
+        } ),
+
+    
+    init = function() {
+
+        initEvents();
+
+    },
+    
+    initEvents = function() {
+
+                            // add navigation events
+    $navArrows.children( ':last' ).on( 'click', function() {
+
+        slitslider.next();
+        return false;
+
+    } );
+
+    $navArrows.children( ':first' ).on( 'click', function() {
+
+        slitslider.previous();
+        return false;
+
+    } );
+
+                        
+    $nav.each( function( i ) {
+        $( this ).on( 'click', function( event ) {
+            var $dot = $( this );
+
+            if( !slitslider.isActive() ) {
+
+                $nav.removeClass( 'nav-dot-current' );
+
+                $dot.addClass( 'nav-dot-current' );
+
+            }
+
+            slitslider.jump( i + 1 );
+
+             return false;
+
+        } );
+
+
+                        
+    } );
+
+                
+    };
+
+        return { init : init };
+
+})();
+
+                
+Page.init();
+
+                /**
+                 * Notes: 
+                 * 
+                 * example how to add items:
+                 */
+
+                /*
+                
+                var $items  = $('<div class="sl-slide sl-slide-color-2" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1"><div class="sl-slide-inner bg-1"><div class="sl-deco" data-icon="t"></div><h2>some text</h2><blockquote><p>bla bla</p><cite>Margi Clarke</cite></blockquote></div></div>');
+                
+                // call the plugin's add method
+                ss.add($items);
+
+                */
+
+            });
